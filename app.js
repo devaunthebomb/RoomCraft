06bf199c076c1c04b9b5f94bd9154daf649ddf4e
TXT-42 <txt-42@TXT-42.local>
2021-07-28 14:06:30 -0700

    data uploaded to mongoDb is now visible in saved projects tab

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const methodOverride = require('method-override')
 const cors = require('cors');
+const cloudinary = require("cloudinary")
 
 const app = express();
 
+const mockModel = require("./mocks/mockprojects");
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -17,9 +19,14 @@ app.get('/items', function(req,res){
     res.sendFile('mockitems.json',{ root: './mocks'})
 })
 
-app.get('/projects', function(req,res){
+
+
+
+app.get('/mockProjects', function(req,res){
     console.log("get request for projects made succssefully");
     res.sendFile('mockprojects.json',{ root: './mocks'})
 })
+
+
 app.listen(process.env.PORT || 8080);
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from '../services/data.service';
 import { Project } from '../services/project'
+import { UploadService } from '../services/upload.service'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-tab1',
@@ -10,15 +12,39 @@ import { Project } from '../services/project'
 })
 export class Tab1Page {
   projectsArr : Project[] = [];
-  // test: any = []
-  constructor(private dataService : DataService) {}
-  ngOnInit() {
-    this.dataService.getProjects().subscribe((data: Project[]) =>
-    {
-      console.log(data);
-      this.projectsArr = data;
-    });
-    // this.test = this.dataService.addingPhotos
-    // console.log(this.test[0].webViewPath)
+  // public projectsList:any[] = [];
+  // subcription: Subscription;
+  test: any = []
+  
+  display: any;
+  constructor(private dataService : DataService, private uploadService: UploadService) {
+    // console.log(this.uploadService.listOfProjectsObs)
+    // this.subcription = this.uploadService.listOfProjectsObs.subscribe((data)=>{
+    //   this.projectsList.push(data);
+    //   console.log(data);
+    // })
+  }
+   ngOnInit() {
+     this.uploadService.getAllProjects().subscribe((data: any)=>{
+       console.log(data);
+       this.display = data;
+       console.log(this.display.documents)
+     })
+
+
+    
+//     this.dataService.getProjects().subscribe((data: Project[]) =>
+//     {
+//       console.log(data);
+//       this.projectsArr = data;
+//     });
+//     this.test = this.dataService.addingPhotos
+//     console.log(this.test[0].webViewPath)
 }
+
+// listenToNewProjects(){
+//    this.uploadService.populateProjects();
+//    console.log(this.projectsList)
+// }
+
 }
